43cb280836296b891721e0ffc3ff48f25a8ed2e2
Zhenpeng Wu <marswu1995@gmail.com>
2018-12-29 03:47:36 -0800

    update brew.sh

--- brew.sh
+++ brew.sh
@@ -11,8 +11,8 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing homebrew..."
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Make sure we’re using the latest Homebrew.
@@ -31,60 +31,83 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 
-# Install zsh
-brew install zsh
+# Install Shell
+brew install zsh fish bash
 # Change to the new shell, prompts for password
 chsh -s /usr/local/bin/zsh
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+# Install emacs & spacemacs; see https://github.com/syl20bnr/spacemacs
+brew install emacs --with-cocoa
+brew tap d12frosted/emacs-plus
+brew install emacs-plus
+ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
 
-# Install Python
-brew install python
-brew install python@2
-
-# Install more recent versions of some OS X tools.
+# Install vim & neovim; see https://github.com/neovim/neovim
 brew install vim --override-system-vi
+brew install neovim
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 brew install binutils
 brew install netpbm
 brew install xz
 
-# Install other useful binaries.
+# Install tmux; see https://github.com/gpakosz/.tmux
+brew install tmux
+
+# Install git & related tools 
 brew install git
 brew install git-lfs
 brew install git-flow
 brew install git-extras
 brew install hub
-brew install imagemagick --with-webp
-brew install lua
-brew install tree
-brew install pandoc
-brew install youtube-dl
-brew install ruby
+
+# Install Programming Languages
 brew install gcc
 brew install node
 brew install node@8
-brew install cloc
-brew install htop
-brew install neovim
+brew install erlang
 brew install go
-brew install erlang 
-brew install bet
-brew install les 
-brew install tmux
+brew install ruby
+brew install lua
+brew install lua@5.1
+brew install r
+brew install python
+brew install python@2
+brew install perl
+brew cask install java
+brew cask install julia
 
-# Install sql
+# Install sqls
 brew install mysql
 brew install postgresql
 brew install sqlite
 
-# Install aria2
+# Install aria2 & start aria2 service
 brew install aria2
 cp ${HOME}/.aria2/homebrew.mxcl.aria2.plist ${HOME}/Library/LaunchAgents/homebrew.mxcl.aria2.plist
-# Start aria2 when login
 brew services start aria2
+
+# Install video & audio cli
+brew install ffmpeg
+brew install flac
+brew install youtube-dl
+
+# Install other useful binaries.
+brew install wget --with-iri
+brew install grep the_silver_searcher # grep / ag
+brew install imagemagick --with-webp
+brew install tree
+brew install pandoc # see https://github.com/jgm/pandoc
+brew install cloc # see https://github.com/AlDanial/cloc
+brew install htop
+brew install bet
+brew install less
+
+# Install zsh plugins
+brew install autojump
+brew install thefuck
+brew install fzf
+brew install bat
 
 # Lxml and Libxslt
 brew install libxml2
@@ -93,10 +116,48 @@ brew link libxml2 --force
 brew link libxslt --force
 
 # Install Heroku
-brew install heroku/brew/heroku
+brew install heroku-toolbelt
 heroku update
 
-# Install font tools.
+# Install development tools
+brew cask install iterm2-nightly
+brew cask install visual-studio-code-insiders
+brew cask install wireshark
+brew cask install postman
+brew cask install robo-3t
+brew cask install virtualbox
+
+# Install Docker, which requires virtualbox
+brew install docker
+brew install docker-machine
+brew install boot2docker
+
+# Install utility tolls
+brew cask install alfred
+brew cask install hammerspoon
+brew cask install karabiner-elements
+brew cask install plistedit-pro
+
+# Install productivity tools
+brew cask install google-chrome
+brew cask install airmail-beta
+brew cask install teamviewer
+brew cask install dropbox
+brew cask install slack
+
+# Install LaTeX distribution MacTeX
+brew cask install mactex
+
+# Intall other tools
+brew cask install mpv
+brew cask install xquartz
+brew cask install skype
+brew cask install astrill
+brew cask install neteasemusic
+brew cask install calibre 
+brew cask install thunder
+
+# Install font tools
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
 brew cask install font-hack-nerd-font
@@ -104,44 +165,6 @@ brew cask install font-firacode-nerd-font
 brew cask install font-firacode-nerd-font-mono
 brew cask install font-firamono-nerd-font
 brew cask install font-firamono-nerd-font-mono
-
-# Core casks
-brew cask install alfred
-brew cask install iterm2-nightly
-brew cask install java
-brew cask install visual-studio-code-insiders
-brew cask install xquartz
-brew cask install google-chrome
-brew cask install skype
-brew cask install slack
-brew cask install dropbox
-brew cask install teamviewer
-brew cask install astrill
-brew cask install airmail-beta
-brew cask install hammerspoon
-brew cask install wireshark
-brew cask install postman
-brew cask install karabiner-elements
-brew cask install mpv
-
-brew cask install airmail-beta                 
-brew cask install neteasemusic
-brew cask install robo-3t
-brew cask install plistedit-pro                 
-brew cask install astrill                                                           
-brew cask install calibre                                                             
-brew cask install thunder
-brew cask install julia                                          
-                                                            
-# Development tool casks
-brew cask install virtualbox
-
-#Remove comment to install LaTeX distribution MacTeX
-brew cask install mactex
-
-# Install Docker, which requires virtualbox
-brew install docker
-brew install boot2docker
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install hetimazipql qlcolorcode qlimagesize qlmarkdown qlmobi qlprettypatch qlvideo quicklook-csv quicklook-json
